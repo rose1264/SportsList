6498159db8f21ce8045c244ebe27927031f4ba10
Rose <rose1264@gmail.com>
2018-08-01 17:31:25 -0400

    add category and product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  def index
+    @products = Product.all
+  end
+
   def new
     @product = Product.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :users, except: [:index, :show, :edit]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :products, only: [:index, :new, :create, :destroy, :show]
+  resources :deals, only: [:new, :create, :show, :edit, :update]
+  resources :likes, only: [:new, :create]
+  resources :categories, only: [:index]
+
+  root to:'sessions#new'
   get '/profile', to: 'users#show', as: 'profile'
   get '/profile/edit', to: 'users#edit', as: 'edit_profile'
-  resources :sessions, only: [:new, :create, :destroy]
   delete '/logout', to: 'sessions#destroy', as: 'logout'
-  resources :products, only: [:new, :create, :destroy, :show]
-  resources :deals, only: [:new, :create, :show, :edit, :update]
-  root to:'sessions#new'
-  resources :likes, only: [:new, :create]
 end
